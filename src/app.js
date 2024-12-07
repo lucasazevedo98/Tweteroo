01ad819ac3353b8027173bda1de8428c09092de2
lucasazevedo98 <azevedi22@gmail.com>
2024-12-07 09:04:36 -0300

    conexão com o DB

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 import express from "express"
+import dotenv from "dotenv"
+import { MongoClient } from "mongodb"
 
+
+dotenv.config()
 
 const app = express()
+const client = new MongoClient(process.env.MONGODB_URL)
+
+let db;
 
 
-app.get("/", (req,res)=>{
-    res.send("olaaaa")
-})
+async function conexaoDB() {
+    try{
+        await client.connect()
+        db = client.db()
+        console.log("Conexão com o DB feita com sucesso!!")
+    } catch (error) {
+        console.log("Erro ao conectar com o MongoDB", error)
+        process.exit(1)
+    }
+} 
+
+conexaoDB()
 
 
-app.listen(5000,()=>{
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
     console.log("servidor rodando!!!")
 })
